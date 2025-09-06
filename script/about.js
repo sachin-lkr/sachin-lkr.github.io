58e7b589be3d10d5c6d98bd93ad89a2380c917b6
sachin <sachin9973083181@gmail.com>
2025-09-06 07:15:23 +0530

    first commit from local portfolio

---
+++ script/about.js
@@ -0,0 +1,224 @@
+const aboutTabs=document.querySelectorAll (".tab");
+
+const aboutContent = document.querySelectorAll(".tab-content");
+
+document.addEventListener('DOMContentLoaded',()=> {
+    if(aboutTabs){
+        aboutTabs[0].click();
+    }
+});
+
+aboutTabs.forEach((tab)=>{
+    tab.addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        aboutTabs.forEach((a)=>{
+            a.classList.remove("active");
+        });
+
+        tab.classList.add("active");
+
+        aboutContent.forEach((c)=>{
+            c.classList.remove("active");
+        });
+
+        const activeTab= tab.dataset.section;
+        document.getElementById(activeTab).classList.add("active");
+
+        if(activeTab=== "experience"){
+            const experience=document.querySelector(".experience-list");
+
+            const experienceList = [
+              {
+          id: 1,
+          date: "Looking for Freelance Opportunities",
+          position: "Freelance Web Developer",
+          company: "Open to Collaborations & Freelance Work",
+          details:
+            "I’m a passionate web developer (MERN Stack / Frontend / Backend) currently open for freelance projects. I help businesses and individuals build fast, responsive, and user-friendly websites.  Developed custom websites for local businesses using HTML, CSS, JavaScript.",
+        },
+        {
+          id: 2,
+          date: "2024 - 2025",
+          position: "MERN Stack Developer",
+          company: "Learning Complete (June-2025)",
+          details:
+            "Worked on full-stack web applications using the MERN stack. Gained hands-on experience in authentication, REST APIs, and MongoDB schemas.",
+        },
+       {
+          id: 3,
+          date: "2023 - 2024",
+          position: "Frontend Developer",
+          company: "Self-Employed",
+          details:
+            "Built responsive and optimized UIs using React.js and Tailwind CSS. Collaborated with backend teams to integrate APIs and improve user experience.",
+        },
+        
+        
+            ];
+
+            const experienceContent= experienceList.map((ele)=>{
+                return`
+                <div class="experience-box" key=${ele?.id}>
+                        <h4>${ele?.date}</h4>
+                        <h3>${ele?.position}</h3>
+                        <div class="company-name">
+                            <span></span>
+                            <p>${ele?.company}</p>
+                        </div>
+                        <p>${ele?.details}</p>
+                    </div>
+                
+                `;
+            }).join(" ");
+
+            if(experience){
+                experience.innerHTML= experienceContent;
+            }
+        }else if(activeTab==="education"){
+           const education = document.querySelector(".education-list");
+
+           const educationList=[
+               {
+            id: 1,
+          date: "2023 - 2026",
+          degree: "Bachelor of Computer Applications (BCA)",
+          institution: "Internation college Ghosait, Lakhisari (Munger university)",
+          details:
+            "Studied core subjects like Data Structures, Web Development, and Operating Systems. Built multiple academic projects using JavaScript and the MERN stack.",
+        },
+        {
+          id: 2,
+          date: "2017 - 2019",
+          degree: "Higher Secondary Education (PCM - Science)",
+          institution: "BSEB(S.R.S +2 H/S Persaban, Lakhisarai)",
+          details:
+            "Focused on Physics, Chemistry, and Mathematics. Developed a strong foundation in logical thinking and problem-solving.",
+        },
+        {
+          id: 3,
+          date: "2016 - 2017",
+          degree: "Secondary School Certificate (10th)",
+          institution: "BSEB(U.M.S. Surari (Lakhisarai))",
+          details:
+            "Completed basic schooling with distinction. Actively participated in computer clubs and tech-related events.",
+        },
+         ];
+
+         const educationContent= educationList.map((ele)=>{
+            return`
+             <div class="experience-box" key=${ele?.id}>
+                        <h4>${ele?.date}</h4>
+                        <h3>${ele?.degree}</h3>
+                        <div class="company-name">
+                            <span></span>
+                            <p>${ele?.institution}</p>
+                        </div>
+                        <p>${ele?.details}</p>
+                    </div>
+             `
+         }).join(" ");
+
+         if(education){
+            education.innerHTML=educationContent;
+         }
+        }else if(activeTab==="skills"){
+            const skills = document.querySelector(".skill-list");
+
+            const skillList=[
+                {
+          id: 1,
+          name: "HTML - Hyper Text Markup Language",
+          icon: "assets/skills/html.png",
+        },
+        {
+          id: 2,
+          name: "CSS - Cascading Style Sheet",
+          icon: "assets/skills/css.png",
+        },
+        {
+          id: 3,
+          name: "JavaScript",
+          icon: "assets/skills/js.png",
+        },
+        {
+          id: 4,
+          name: "MongoDB",
+          icon: "assets/skills/mongodb.png",
+        },
+        {
+          id: 5,
+          name: "Bootstrap",
+          icon: "assets/skills/bootstrap.png",
+        },
+        {
+          id: 6,
+          name: "Node JS",
+          icon: "assets/skills/node.png",
+        },
+        {
+          id: 7,
+          name: "React JS",
+          icon: "assets/skills/react.png",
+        },
+         ];
+
+         const skillContent = skillList.map((ele)=>{
+            return`
+            <div class="skill-box" key=${ele?.id}>
+                        <img src=${ele?.icon} 
+                        alt=${ele?.name}
+                        title=${ele?.id}
+                        loading="lazy">
+                    </div>
+            `
+         }).join("");
+
+         if(skills){
+            skills.innerHTML=skillContent;
+         }
+        }else if(activeTab==="about-me"){
+            const myInfo= document.querySelector(".my-info");
+
+            const infoList=[
+
+                {
+          id: 1,
+          key: "Name : ",
+          value: "Sachin Kumar",
+        },
+        {
+          id: 2,
+          key: "Country : ",
+          value: "India",
+        },
+        {
+          id: 3,
+          key: "Industry : ",
+          value: "Software & IT",
+        },
+        {
+          id: 4,
+          key: "Experience : ",
+          value: "Fresher",
+        },
+        {
+          id: 5,
+          key: "Address : ",
+          value:"Vill-Bakiya bad, Post- Surari Imamnagar, PS- halsi, Dist-Lakhisarai, State-Bihar(811107)"
+        },
+            ];
+            const infoContent = infoList.map((ele)=>{
+                return`
+                <div class="info-box" key=${ele?.id}>
+                <span>${ele?.key}</span>
+                <span>${ele?.value}</span>
+                </div>
+                `
+            }).join("");
+            if(myInfo){
+                myInfo.innerHTML=infoContent;
+            }
+        }
+    });
+});
---
+++ script/projects.js
@@ -0,0 +1,109 @@
+const projectList = [
+  {
+    id: 1,
+    number: "01",
+    dev:"Front-end Project.",
+    title: "Weather-app projects",
+    description:
+      "A responsive Weather App that provides real-time weather updates including temperature, humidity, wind speed, and conditions using HTML, CSS, JavaScript & OpenWeatherMap API.!",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    image: "assets/projects/weather-apps.JPG",
+    liveLink: "https://sachin-lkr.github.io/weather-app/",
+    githubLink: "https://github.com/sachin-lkr/weather-app",
+  },
+  {
+    id: 2,
+    number: "02",
+    title: "Calculator",
+    description:
+      "This project is a responsive calculator application created with HTML, CSS, and JavaScript. It allows users to perform fundamental arithmetic operations including addition, subtraction, multiplication, and division. The design is minimalistic with a modern UI, and the project demonstrates core JavaScript concepts such as DOM manipulation, event handling, and dynamic updates. It is fully responsive, ensuring smooth functionality on both desktop and mobile devices.",
+    techStack: ["HTML", "CSS" , "ANd JavaScript"],
+    image: "assets/projects/colculator.jpg",
+    liveLink: "#",
+    githubLink: "#",
+  },
+];
+
+const projects= document.querySelector(".projects");
+let currentIndex =0;
+const renderProject =(index)=>{
+    const projectContent= projectList[index];
+
+    const previousDisabled = currentIndex===0;
+
+    const nextDisabled = currentIndex===projectList.length-1;
+
+    projects.innerHTML= `
+    <div class="projects-info">
+                    <h2>Front-end
+                      <span>Projects</span>
+                    </h2>
+                    <h3>${projectContent?.number}</h3>
+                    <h4>${projectContent?.title}</h4>
+                    <p>
+                        ${projectContent?.description}
+                    </p>
+                    <div class="tech-stack">
+                        ${projectContent?.techStack?.map((tech, i)=>{
+                          return`
+                          <span key =${i}>${tech} </span>`;
+                        })}
+                        
+
+                    </div>
+                    <hr/>
+                    <div class="links">
+                        <div class="live">
+                          <a href=${projectContent?.liveLink} target="_blank">
+                             <i class="fa-solid fa-arrow-right"></i>
+                              
+                          </a>
+                           <p> Live </p>
+                        </div>
+                        <a href=${projectContent.githubLink} target="_blank">
+                         <i class="fa-brands fa-github"></i>
+                         
+                        </a>
+                    </div>
+                </div>
+
+                <div class="right-section">
+                    <a href=${projectContent?.liveLink} target="_blank">
+                     <img src=${projectContent.image} alt="project" title="Click-here">
+                    </a>
+                    
+                    <div class="arrows">
+                        <a href="#" id="previous" class=${previousDisabled? "disabled-btn":""}><i class="fa-solid fa-arrow-left"></i></a>
+                        <a href="#" id="next" class=${nextDisabled? "disabled-btn":""}><i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                </div>
+
+
+
+
+    `;
+
+    document.getElementById("previous").addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        if(currentIndex>0){
+          currentIndex--;
+          renderProject(currentIndex);
+        }
+    });
+
+     document.getElementById("next").addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        if(currentIndex<projectList.length-1){
+          currentIndex++;
+          renderProject(currentIndex);
+        }
+    });
+   
+
+    
+  
+};
+renderProject(currentIndex);
+
